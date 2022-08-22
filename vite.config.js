81e3e75d8c6731b734aeffaff71d642d29aedca7
Marco Roth <marco.roth@intergga.ch>
2022-08-22 21:25:50 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 export default {
-  resolve: {
-    alias: {
-      '@hotwired/turbo': path.resolve(__dirname, '../dist/turbo.es2017-esm.js')
-    }
-  }
+  // resolve: {
+  //   alias: {
+  //     '@hotwired/turbo': path.resolve(__dirname, '../dist/turbo.es2017-esm.js')
+  //   }
+  // }
 }
